3f5d31311ef01d05d2158992e57c040f2657e34d
R2D2Dan <Danil150799@gmail.com>
2022-03-17 15:25:21 +0300

    add space

--- Solid/Solid.go
+++ Solid/Solid.go
@@ -5,14 +5,20 @@ import (
 )
 
 func Solid(text string, key string) string {
-	alphabet := "абвгдеёжзийклмнопрстуфхцчшщъыьэюя "
+	alphabet := "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 	alphabetrune := []rune(alphabet)
 	keyrune := []rune(key)
 	res := ""
 	for i, k := range text {
-		textindex := strings.Index(alphabet, string(k))/2 + (strings.Index(alphabet, string(k)) % 2)
-		keyindex := strings.Index(alphabet, string(keyrune[(i/2)%len(keyrune)])) / 2
-		res = res + string(alphabetrune[(textindex+keyindex)%len(alphabetrune)])
+		if string(k) == " " {
+			res = res + " "
+			continue
+		} else {
+			textindex := strings.Index(alphabet, string(k))/2 + (strings.Index(alphabet, string(k)) % 2)
+			keyindex := strings.Index(alphabet, string(keyrune[(i/2)%len(keyrune)])) / 2
+			res = res + string(alphabetrune[(textindex+keyindex)%len(alphabetrune)])
+		}
+
 	}
 
 	return res
